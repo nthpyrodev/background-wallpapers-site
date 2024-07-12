320a8dc3cac4e086a6d585f4a227e4d11398e960
Nicholas Martinez <hi@nthpyro.dev>
2024-07-12 17:25:47 +0100

    Create footer.php

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+    <footer class="mt-5 py-3 bg-light">
+        <div class="container text-center">
+            <p>Background Wallpapers</p>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="./assets/js/main.js"></script>
+</body>
+</html>
